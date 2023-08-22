e82202e4e0dc614f0f968d842dee8fa006155911
Behruz-08 <134039054+Behruz-08@users.noreply.github.com>
2023-08-22 21:18:58 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NewTask from "./components/NewTask/NewTask";
+
+function App() {
+  return (
+    <div className="App">
+      <NewTask />
+    </div>
+  );
+}
+
+export default App;
